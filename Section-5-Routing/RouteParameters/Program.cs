13695ec16f23ea98cf3b3eca5c99480337f5052a
Tresten Pool <trestenp@cctexas.com>
2024-11-20 17:19:06 -0600

    Added to routing

--- Section-5-Routing/RouteParameters/Program.cs
+++ Section-5-Routing/RouteParameters/Program.cs
@@ -4,7 +4,56 @@ namespace RouteParameters {
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            // enable routing
+            app.UseRouting();
+
+            /*
+                no longer have to nest inside of app.UseEndpoints.
+                Instead it is recommended to define at the top level like the example below this comment
+
+                app.UseEndpoints(endpoints => {
+                    endpoints.MapGet("files/{filename}.{extension}", async (context) => {
+                    await context.Response.WriteAsync("In files");
+                    });
+                });
+            */
+
+            #pragma warning disable
+            app.UseEndpoints(endpoints => {
+                // files
+                endpoints.Map("/files/{filename}.{extension}", async (context) => {
+                    string filename = context.Request.RouteValues["filename"].ToString();
+                    string extension = context.Request.RouteValues["extension"].ToString();
+                    await context.Response.WriteAsync($"In files {filename} {extension}\n");
+                });
+
+                // employees
+                endpoints.Map("employee/profile/{employeename=TrestenPool}", async (context) => {
+                    string empName = context.Request.RouteValues["employeename"].ToString();
+                    await context.Response.WriteAsync($"In employee profile {empName}\n");
+                });
+
+                // products
+                endpoints.MapGet("products/details/{id:int?}", async context => {
+                    string? id = null;
+                    if(context.Request.RouteValues.ContainsKey("id")) {
+                        id = context.Request.RouteValues["id"].ToString();
+                    }
+                    await context.Response.WriteAsync($"In products {id ?? "no id provided"}\n");
+                });
+
+            });
+            #pragma warning enable
+
+            // defining routes
+            //app.MapGet("files/{filename}.{extension}", async (context) => {
+            //    await context.Response.WriteAsync("In files");
+            //});
+
+            // this middleware will catch all other routes that are not listed above
+            app.Run(async (context) => {
+                await context.Response.WriteAsync($"Request received at {context.Request.Path}");
+            });
 
             app.Run();
         }
