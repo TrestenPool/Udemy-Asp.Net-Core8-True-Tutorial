a8b8dcae26bda9b43af82493659b9bb7ff019ae0
Tresten Pool <trestenp@cctexas.com>
2024-11-21 17:38:25 -0600

    Added to route constraints

---
+++ Section-5-Routing/RouteParameters/Custom Constraints/MonthsCustomConstraint.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace RouteParameters.Custom_Constraints {
+    public class MonthsCustomConstraint : IRouteConstraint {
+        // interface method we have to implement
+        public bool Match(
+                // our context
+                HttpContext? httpContext, 
+                // the route we are currently checking if we match
+                IRouter? route, 
+                // not sure
+                string routeKey, 
+                // route value dictionary
+                RouteValueDictionary values, 
+                // route direction: for an incoming request or for the generation of a url
+                RouteDirection routeDirection) {
+
+            // checks if the 
+            if(!values.ContainsKey(routeKey)) {
+                return false;
+            }
+
+            // check if our month value matches our regex
+            Regex regex = new Regex($"^(apr|jul|oct|jan)$");
+            string? monthValue = Convert.ToString(values[routeKey]);
+
+            if(regex.IsMatch(monthValue ?? "")) {
+                return true;
+            }
+            else {
+                return false;
+            }
+
+        }
+    }
+}
--- Section-5-Routing/RouteParameters/Program.cs
+++ Section-5-Routing/RouteParameters/Program.cs
@@ -1,7 +1,15 @@
+using RouteParameters.Custom_Constraints;
+
 namespace RouteParameters {
     public class Program {
         public static void Main(string[] args) {
             var builder = WebApplication.CreateBuilder(args);
+
+            // add the custom constraint
+            builder.Services.AddRouting(options => {
+                options.ConstraintMap.Add("months", typeof(MonthsCustomConstraint));
+            });
+
             var app = builder.Build();
 
             // enable routing
@@ -42,6 +50,42 @@ namespace RouteParameters {
                     await context.Response.WriteAsync($"In products {id ?? "no id provided"}\n");
                 });
 
+                // guid
+                endpoints.MapGet("cities/{cityid:guid}", async context => {
+                    string? cityid = null;
+                    if(context.Request.RouteValues.ContainsKey("cityid")) {
+                        cityid = context.Request.RouteValues["cityid"].ToString();
+                    }
+                    await context.Response.WriteAsync($"city id = {cityid}");
+                });
+
+                // length
+                endpoints.MapGet("company/{abrev:length(2,3):alpha}", async context => {
+                    string? abrev = null;
+                    if(context.Request.RouteValues.ContainsKey("abrev")) {
+                        abrev = context.Request.RouteValues["abrev"].ToString();
+                    }
+                    await context.Response.WriteAsync($"city id = {abrev}");
+                });
+
+                // regex, accept an ip address
+                endpoints.MapGet("ip/{ipaddr:regex(^\\d{{1,3}}.\\d{{1,3}}.\\d{{1,3}}.\\d{{1,3}}$)}", async context => {
+                    string? ipaddr = null;
+                    if(context.Request.RouteValues.ContainsKey("ipaddr")) {
+                        ipaddr = context.Request.RouteValues["ipaddr"].ToString();
+                    }
+                    await context.Response.WriteAsync($"ip address = {ipaddr}");
+                });
+
+                // custom constraint
+                endpoints.MapGet("report/{year:int:min(1900)}/{month:months}", async context => {
+                    int? year = Convert.ToInt32(context.Request.RouteValues["year"]);
+                    string? month = Convert.ToString(context.Request.RouteValues["month"]);
+
+                    await context.Response.WriteAsync($"year = {year}\nmonth = {month}");
+                });
+
+                
             });
             #pragma warning enable
 
