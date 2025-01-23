50031a4352840d7b9203fdeb631611d4feaefaca
Tresten Pool <trestenpool@gmail.com>
2025-01-22 20:00:21 -0600

    Added to Section 14

--- Section-14-Configuration/HttpRequestExample/Program.cs
+++ Section-14-Configuration/HttpRequestExample/Program.cs
@@ -1,6 +1,7 @@
 using HttpRequestExample.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllersWithViews();
 
 // Registers IHttpClientFactory
@@ -15,6 +16,11 @@ builder.Services.AddHttpClient();
 builder.Configuration.AddJsonFile("customConfig.json", optional: true);
 
 var app = builder.Build();
+
+// must provide api key
+if( app.Configuration["ApiKey"] == null ) {
+  throw new Exception("You must provide a key 'ApiKey' in the appsettings.json file");
+}
 
 app.MapControllers();
 app.UseRouting();
--- Section-14-Configuration/HttpRequestExample/Services/MyService.cs
+++ Section-14-Configuration/HttpRequestExample/Services/MyService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -9,13 +10,17 @@ namespace HttpRequestExample.Services;
 public class MyService{
   // fields
   private readonly IHttpClientFactory _httpClientFactory;
+  private readonly IConfiguration _configuration;
 
   // constructor
-  public MyService(IHttpClientFactory httpClientFactory) {
+  public MyService(IHttpClientFactory httpClientFactory, IConfiguration configuration) {
     _httpClientFactory = httpClientFactory;
+    _configuration = configuration;
   }
 
-  public async Task Method() {
+  public async Task<Dictionary<string,object>?> Method() {
+
+    string apiToken = _configuration["ApiKey"] ?? "";
 
     // make the http request using the factory to create a client for us
     using(HttpClient client = _httpClientFactory.CreateClient() ) {
@@ -24,16 +29,32 @@ public class MyService{
       HttpResponseMessage message = await client.SendAsync(
         new HttpRequestMessage(
           HttpMethod.Get, 
-          "https://dog.ceo/api/breeds/list/all"
+          $"https://finnhub.io/api/v1/stock/market-status?exchange=US&token={apiToken}"
         )
       );
 
       // read the response
-      var content = await message.Content.ReadAsStringAsync();
+      string content = await message.Content.ReadAsStringAsync();
 
-      
+      // store the json content in a dictionary
+      var dictionary = JsonSerializer.Deserialize<Dictionary<string,object>>(content);
 
-      Console.WriteLine(content);
+      return dictionary;
+
+      // // there was no response from the api
+      // if(responseDictionary == null) {
+      //   throw new InvalidOperationException("No response from api server");
+      // }
+
+      // // The api sent back an error
+      // if(responseDictionary.ContainsKey("error")) {
+      //   throw new InvalidOperationException(
+      //     Convert.ToString(responseDictionary["error"])
+      //   );
+      // }
+
+      // // return the dictionary to the user
+      // return responseDictionary;
     }
 
   }
