fb9b4bbd94e363cd153e1d561d698c55a7061795
Tresten Pool <trestenp@cctexas.com>
2025-01-28 16:55:35 -0600

    Added to Section 15

--- Section-15-XUnit/CRUDTests/CountriesServiceTest.cs
+++ Section-15-XUnit/CRUDTests/CountriesServiceTest.cs
@@ -1,24 +1,74 @@
 using ServiceContracts;
+using Services;
 
 namespace CRUDTests;
 
 public class CountriesServiceTest{
+  // Properties
   private readonly ICountriesService _countriesService;
 
+  // constructor
   public CountriesServiceTest() {
+    _countriesService = new CountriesService();
   }
 
-  [Fact]
-  public void Test1(){
+  // enum used for test
+  public enum AddCountry {
+    Null_CountryAddRequest,
+    Null_CountryName,
+    Duplicate_CountryName,
+    Success_CountryAddRequest
+  }
+
+  // our test
+  [Theory]
+  [InlineData(AddCountry.Null_CountryAddRequest)]
+  [InlineData(AddCountry.Null_CountryName)]
+  [InlineData(AddCountry.Duplicate_CountryName)]
+  [InlineData(AddCountry.Success_CountryAddRequest)]
+  public void AddCountry_Test(AddCountry addCountryOption) {
+
     // Arrange
-    MyMath mm = new MyMath();
-    int input1 = 10, input2 = 20;
-    int expected = 30;
+    CountryAddRequest? request;
 
-    // Act
-    int actual = mm.Add(input1, input2);
+    Type exeptionType = typeof(ArgumentException);
 
-    // Assert
-    Assert.Equal(expected, actual);
+    // switch based off of inlinedata
+    switch(addCountryOption) {
+
+      case AddCountry.Null_CountryAddRequest:
+        request = null;
+        Assert.Throws<ArgumentNullException>(() => {
+          _countriesService.AddCountry(request);
+        });
+        break;
+
+      case AddCountry.Null_CountryName:
+        request = new CountryAddRequest(){CountryName = null};
+        Assert.Throws<ArgumentException>(() => {
+          _countriesService.AddCountry(request);
+        });
+        break;
+
+      case AddCountry.Duplicate_CountryName:
+        request = new CountryAddRequest(){CountryName = "USA"};
+        Assert.Throws<ArgumentException>(() => {
+          _countriesService.AddCountry(request);
+          _countriesService.AddCountry(request);
+        });
+        break;
+
+      case AddCountry.Success_CountryAddRequest:
+        request = new CountryAddRequest(){CountryName = "USA"};
+        CountryResponse response = _countriesService.AddCountry(request);
+        Assert.True(response.CountryId != Guid.Empty);
+        break;
+
+      default:
+        break;
+    }
+
+
   }
+  
 }
--- Section-15-XUnit/Services/CountriesService.cs
+++ Section-15-XUnit/Services/CountriesService.cs
@@ -1,12 +1,29 @@
-﻿using ServiceContracts;
+﻿using Entities;
+using ServiceContracts;
 
 namespace Services;
 
 public class CountriesService : ICountriesService{
+  // private field
+  private readonly List<Country> _countries;
+
+  // constructor
+  public CountriesService() {
+    _countries = new List<Country>();
+  }
 
   // implemented interface
   public CountryResponse AddCountry(CountryAddRequest? countryAddRequest){
-    throw new NotImplementedException();
+
+    ArgumentNullException.ThrowIfNull(countryAddRequest);
+
+    if( string.IsNullOrEmpty(countryAddRequest.CountryName) ){
+      throw new ArgumentException("CountryName can't be null");
+    }
+
+    Country country = countryAddRequest.ToCountry();
+
+    return null;
   }
 
 }
