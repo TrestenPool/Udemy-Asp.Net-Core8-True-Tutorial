b6bd6613d4ff47c5e79d9e952b25248cbd58987f
Tresten Pool <trestenp@cctexas.com>
2025-02-06 16:57:22 -0600

    added to 15

--- Section-15-XUnit/CRUDTests/PersonServiceTest.cs
+++ Section-15-XUnit/CRUDTests/PersonServiceTest.cs
@@ -1,16 +1,19 @@
 using Entities;
 using ServiceContracts;
 using Services;
+using Xunit.Abstractions;
 
 namespace CRUDtests;
 
 public class PersonServiceTest {
   // property
   private readonly IPersonService _personService;
+  private readonly ITestOutputHelper _outputHelper;
 
   // constructor
-  public PersonServiceTest() {
+  public PersonServiceTest(ITestOutputHelper testOutputHelper) {
     _personService = new PersonService();
+    _outputHelper = testOutputHelper;
   }
 
   public enum AddPersonTests {
@@ -81,26 +84,26 @@ public class PersonServiceTest {
     switch(personIdOption) {
 
       case PersonIdOptions.NullPersonId:
-      Assert.Throws<ArgumentNullException>(() => {
-        _personService.GetPersonByPersonId(null);
-      });
-      break;
+        Assert.Throws<ArgumentNullException>(() => {
+          _personService.GetPersonByPersonId(null);
+        });
+        break;
 
       case PersonIdOptions.ValidPersonId:
-      personAddRequest = new PersonAddRequest(){
-        PersonName="Tresten",
-        Email="tresten_email@gmail.com",
-        Address="412 North Henderson"
-      };
-      personResponse = _personService.AddPerson(personAddRequest);
-      PersonResponse? actualPerson = _personService.GetPersonByPersonId(personResponse.PersonId);
-      Assert.True(personResponse.PersonId == actualPerson?.PersonId);
-      break;
+        personAddRequest = new PersonAddRequest(){
+          PersonName="Tresten",
+          Email="tresten_email@gmail.com",
+          Address="412 North Henderson"
+        };
+        personResponse = _personService.AddPerson(personAddRequest);
+        PersonResponse? actualPerson = _personService.GetPersonByPersonId(personResponse.PersonId);
+        Assert.True(personResponse.PersonId == actualPerson?.PersonId);
+        break;
 
       case PersonIdOptions.InvalidPersonId:
-      personResponse = _personService.GetPersonByPersonId(Guid.NewGuid());
-      Assert.Null(personResponse);
-      break;
+        personResponse = _personService.GetPersonByPersonId(Guid.NewGuid());
+        Assert.Null(personResponse);
+        break;
     }
 
   }
@@ -117,27 +120,32 @@ public class PersonServiceTest {
     List<PersonResponse> list_of_persons;
     switch(options) {
       case AllPersonsOptions.NoPersons:
-      list_of_persons = _personService.GetAllPersons();
-      Assert.Empty(list_of_persons);
-      break;
+        list_of_persons = _personService.GetAllPersons();
+        Assert.Empty(list_of_persons);
+        break;
 
       case AllPersonsOptions.SomePersons:
-      _personService.AddPerson(
-        new PersonAddRequest(){PersonName="Tresten",Address="110 Notyourbusiness",Email="Tresten@yahoo.com"}
-      );
-      _personService.AddPerson(
-        new PersonAddRequest(){PersonName="John",Address="4513 GetOffMyLawn St",Email="John@hotmail.com"}
-      );
-      list_of_persons = _personService.GetAllPersons();
-      Assert.Collection(list_of_persons, 
-        p => {
-          Assert.Equal("Tresten", p.PersonName);
-        },
-        p => {
-          Assert.Equal("John", p.PersonName);
-        }
-      );
-      break;
+        // add our persons
+        _personService.AddPerson(
+          new PersonAddRequest(){PersonName="Tresten",Address="110 Notyourbusiness",Email="Tresten@yahoo.com"}
+        );
+        _personService.AddPerson(
+          new PersonAddRequest(){PersonName="John",Address="4513 GetOffMyLawn St",Email="John@hotmail.com"}
+        );
+        // get the list of persons
+        list_of_persons = _personService.GetAllPersons();
+        // make sure the persons we added are in the collection
+        Assert.Collection(list_of_persons, 
+          p => {
+            Assert.Equal("Tresten", p.PersonName);
+            _outputHelper.WriteLine("=====\n\nHI MATE\n\n====");
+          },
+          p => {
+            Assert.Equal("John", p.PersonName);
+          }
+        );
+        break;
+
     }
   }
 
